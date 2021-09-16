ed90080052aef007b194137fa1c6922fee10270a
Mohammed Araby <support@hellobye.net>
2021-09-16 12:31:49 +0300

    Init my 1st flutter app #ask_me_anything app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueAccent,
+          appBar: AppBar(
+            title: Text('Ask Me AnyThing'),
+            backgroundColor: Colors.black45,
+          ),
+          body: MyBall(),
+        ),
       ),
     );
+
+class MyBall extends StatefulWidget {
+  @override
+  _MyBallState createState() => _MyBallState();
+}
+class _MyBallState extends State<MyBall> {
+    int ballNumber = 1;
+    void changeBall(){
+      setState(() {
+        ballNumber = Random().nextInt(4) + 1;
+      });
+    }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeBall();
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+
+          )
+        ],
+      ),
+    );
+  }
+}
